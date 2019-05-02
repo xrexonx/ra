d05ef54e1db6f8db7d7eb2660da13407742877d9
Rexon A. De los Reyes <rexondelosreyes88@gmail.com>
2019-05-02 15:40:21 +0800

    Add form

--- src/blog/Blog.js
+++ src/blog/Blog.js
@@ -1,10 +1,80 @@
 import React, { Component } from 'react';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 
 class Blog extends Component {
+
+  initialValues() {
+    return {
+      name: '',
+      email: '',
+      phone: '',
+      password: '',
+    };
+  }
+
+  validate(values) {
+    console.log({values});
+  }
+
+  onSubmit(values, setSubmitting ) {
+    setSubmitting(true);
+    console.log({values});
+    // Submit data to server
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 400);
+  }
+
   render() {
     return (
       <div className="mdl-grid portfolio-max-width">
-        Blag
+        <Formik
+          validate={this.validate}
+          initialValues={this.initialValues()}
+          onSubmit={(values, { setSubmitting }) => this.onSubmit(values, setSubmitting)}
+        >
+          {({isSubmitting}) => (
+            <Form>
+              <div className="mdl-textfield mdl-js-textfield">
+                <Field className="mdl-textfield__input" type="text" name="name" />
+                <label className="mdl-textfield__label" htmlFor="name">Name</label>
+                <span className="mdl-textfield__error">
+                  <ErrorMessage name="name" component="div" />
+                </span>
+              </div>
+              <br/>
+              <div className="mdl-textfield mdl-js-textfield">
+                <Field className="mdl-textfield__input" type="email" name="email" />
+                <label className="mdl-textfield__label" htmlFor="email">Email</label>
+                <span className="mdl-textfield__error">
+                  <ErrorMessage name="email" component="div" />
+                </span>
+              </div>
+              <br/>
+              <div className="mdl-textfield mdl-js-textfield">
+                <Field className="mdl-textfield__input" type="number" name="phone" />
+                <label className="mdl-textfield__label" htmlFor="phone">Phone</label>
+                <span className="mdl-textfield__error">
+                  <ErrorMessage name="phone" component="div" />
+                </span>
+              </div>
+              <br/>
+              <div className="mdl-textfield mdl-js-textfield">
+                <Field className="mdl-textfield__input" type="password" name="password" />
+                <label className="mdl-textfield__label" htmlFor="password">Password</label>
+                <span className="mdl-textfield__error">
+                  <ErrorMessage name="password" component="div" />
+                </span>
+              </div>
+              <br/>
+              <button
+                disabled={isSubmitting}
+                className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                Button
+              </button>
+            </Form>
+          )}
+        </Formik>
       </div>
     );
   }
--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -9,8 +9,8 @@ class Footer extends Component {
         </div>
         <div className="mdl-mini-footer__right-section">
           <ul className="mdl-mini-footer__link-list">
-            <li><a href="#">Help</a></li>
-            <li><a href="#">Privacy & Terms</a></li>
+            <li><a href="/">Help</a></li>
+            <li><a href="/">Privacy & Terms</a></li>
           </ul>
         </div>
       </footer>
