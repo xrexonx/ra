6b5795500da054da89fb3d2854b0bf3a46d7a8ac
Rexon A. De los Reyes <rexondelosreyes88@gmail.com>
2019-05-03 13:03:11 +0800

    Add forms

---
+++ src/blog/Reservation.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+
+class Reservation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2,
+      notes: ''
+    };
+
+    this.submitForm = this.submitForm.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+
+  }
+
+  submitForm(e) {
+    e.preventDefault();
+    if (this.state) {
+      console.log(this.state);
+    }
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          Is going:
+          <input
+            name="isGoing"
+            type="checkbox"
+            checked={this.state.isGoing}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input
+            name="numberOfGuests"
+            type="number"
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange} />
+        </label>
+        <br/>
+        <label>
+          Notes:
+          <input
+            name="notes"
+            type="text"
+            value={this.state.notes}
+            onChange={this.handleInputChange} />
+        </label>
+        <br/>
+        <button
+          onClick={this.submitForm}
+          className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
+        >
+          Button
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Reservation;
